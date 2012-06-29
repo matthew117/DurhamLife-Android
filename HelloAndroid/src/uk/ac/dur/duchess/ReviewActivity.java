3a783d9b388b58d16b68ea36aa5e8f747d8e67ae
matthew117 <matthew117@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-06-29 16:22:54 +0000

    Removed unnecessary imports.

--- HelloAndroid/src/uk/ac/dur/duchess/ReviewActivity.java
+++ HelloAndroid/src/uk/ac/dur/duchess/ReviewActivity.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
