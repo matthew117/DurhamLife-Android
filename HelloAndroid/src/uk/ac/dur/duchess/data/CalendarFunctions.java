f3c8ce435f71033653c66d2d116f2063317b9b71
jamie_bates_8 <jamie_bates_8@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-06-30 18:45:39 +0000

    Fixed calendar functions

--- HelloAndroid/src/uk/ac/dur/duchess/data/CalendarFunctions.java
+++ HelloAndroid/src/uk/ac/dur/duchess/data/CalendarFunctions.java
@@ -2,7 +2,6 @@ package uk.ac.dur.duchess.data;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Calendar;
 
 import uk.ac.dur.duchess.Event;
@@ -10,45 +9,46 @@ import uk.ac.dur.duchess.Review;
 
 public class CalendarFunctions
 {
-	public static String getEventDate(Event e)
+	public static String getEventDate(Event event)
+	{
+		return getEventDate(event.getStartDate(), event.getEndDate());
+	}
+	
+	public static String getEventDate(String start, String end)
 	{
 		try
 		{
 			SimpleDateFormat sourceFormat = new SimpleDateFormat("yyyy-MM-dd");
 			SimpleDateFormat destinationFormat = new SimpleDateFormat("d MMMMM yyyy");
 			
-			Date startDate = sourceFormat.parse(e.getStartDate());
-			Date endDate   = sourceFormat.parse(e.getEndDate());
+			Calendar startDate = Calendar.getInstance(); startDate.setTime(sourceFormat.parse(start));
+			Calendar endDate   = Calendar.getInstance(); endDate.setTime(sourceFormat.parse(end));
 			
-			String _startDate = destinationFormat.format(startDate);
-			String _endDate   = destinationFormat.format(endDate);
+			String _startDate = destinationFormat.format(startDate.getTime());
+			String _endDate   = destinationFormat.format(endDate.getTime());
 			
 			Calendar today     = Calendar.getInstance();
-			Calendar tomorrow  = Calendar.getInstance(); tomorrow.roll(Calendar.DATE, true);
-			Calendar yesterday = Calendar.getInstance(); tomorrow.roll(Calendar.DATE, false);
-			
-			Date _today = today.getTime();
-			Date _tomorrow = tomorrow.getTime();
-			Date _yesterday = yesterday.getTime();
+			Calendar tomorrow  = Calendar.getInstance(); tomorrow.roll(Calendar.DAY_OF_YEAR, true);
+			Calendar yesterday = Calendar.getInstance(); yesterday.roll(Calendar.DAY_OF_YEAR, false);
 						
-			if(startDate.getMonth() == _today.getMonth())
+			if(startDate.get(Calendar.MONTH) == today.get(Calendar.MONTH))
 			{
-				if(startDate.getDate()     == _today.getDate()) _startDate = "Today";
-				if(startDate.getDate() - 1 == _today.getDate()) _startDate = "Tomorrow";
-				if(startDate.getDate() + 1 == _today.getDate()) _startDate = "Yesterday";
+				if(startDate.get(Calendar.DATE)     == today.get(Calendar.DATE)) _startDate = "Today";
+				if(startDate.get(Calendar.DATE) - 1 == today.get(Calendar.DATE)) _startDate = "Tomorrow";
+				if(startDate.get(Calendar.DATE) + 1 == today.get(Calendar.DATE)) _startDate = "Yesterday";
 			}
 			
-			if(endDate.getMonth() == _today.getMonth())
+			if(endDate.get(Calendar.MONTH) == today.get(Calendar.MONTH))
 			{
-				if(endDate.getDate()     == _today.getDate()) _endDate = "Today";
-				if(endDate.getDate() - 1 == _today.getDate()) _endDate = "Tomorrow";
-				if(endDate.getDate() + 1 == _today.getDate()) _endDate = "Yesterday";
+				if(endDate.get(Calendar.DATE)     == today.get(Calendar.DATE)) _endDate = "Today";
+				if(endDate.get(Calendar.DATE) - 1 == today.get(Calendar.DATE)) _endDate = "Tomorrow";
+				if(endDate.get(Calendar.DATE) + 1 == today.get(Calendar.DATE)) _endDate = "Yesterday";
 			}
 			
-			if(isSameDate(startDate, _tomorrow)) _startDate = "Tomorrow";
-			if(isSameDate(endDate, _tomorrow)) _endDate = "Tomorrow";
-			if(isSameDate(startDate, _yesterday)) _startDate = "Yesterday";
-			if(endDate.before(_today))
+			if(isSameDate(startDate, tomorrow)) _startDate = "Tomorrow";
+			if(isSameDate(endDate, tomorrow)) _endDate = "Tomorrow";
+			if(isSameDate(startDate, yesterday)) _startDate = "Yesterday";
+			if(endDate.before(today))
 			{
 				_startDate = "This event has ended";
 				_endDate = "";
@@ -64,33 +64,41 @@ public class CalendarFunctions
 		}
 	}
 	
-	private static boolean isSameDate(Date d1, Date d2)
+	public static boolean isSameDate(Calendar a, Calendar b)
 	{
-		return d1.getMonth() == d2.getMonth() &&
-			   d1.getDate()  == d2.getDate();
+		return a.get(Calendar.YEAR) == b.get(Calendar.YEAR)
+				&& a.get(Calendar.DAY_OF_YEAR) == b.get(Calendar.DAY_OF_YEAR);
 	}
 	
-	public static String getReviewTime(Review r)
+	public static String getReviewTime(Review review)
+	{
+		return getReviewTime(review.getTimestamp());
+	}
+	
+	public static String getReviewTime(String timeStamp)
 	{
 		try
 		{
 			SimpleDateFormat sourceFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			SimpleDateFormat destinationFormat = new SimpleDateFormat("d MMMMM yyyy");
-			
-			Date timeStamp = sourceFormat.parse(r.getTimestamp());
+
 			Calendar today = Calendar.getInstance();
-			
-			
-			int days = today.get(Calendar.DATE) - timeStamp.getDate(); 
-			int hours = today.get(Calendar.HOUR_OF_DAY) - timeStamp.getHours();  
-			int minutes = today.get(Calendar.MINUTE) - timeStamp.getMinutes();
-					
-			if(days == 0 && hours == 0) return minutes + " minutes ago";
-			else if(days == 0 && hours == 1 && minutes < 0) return minutes + " minutes ago";
-			else if(days == 0) return hours + " hours ago";
-			else if(days == 1 && hours < 0) return hours + " hours ago";
-			
-			return destinationFormat.format(timeStamp);
+			Calendar _timeStamp = Calendar.getInstance();
+			_timeStamp.setTime(sourceFormat.parse(timeStamp));
+
+			int days = today.get(Calendar.DATE) - _timeStamp.get(Calendar.DATE); 
+			int hours = today.get(Calendar.HOUR_OF_DAY) - _timeStamp.get(Calendar.HOUR_OF_DAY);  
+			int minutes = today.get(Calendar.MINUTE) - _timeStamp.get(Calendar.MINUTE);
+
+			System.out.println("Days: " + days + ", Hours: " + hours + ", Minutes: " + minutes);
+
+			if(days == 0 && hours == 0 && minutes == 0) return "Less than 1 minute ago";
+			else if(days == 0 && hours == 0) return minutes + " minute" + ((minutes != 1) ? "s" : "") + " ago : 1";
+			else if(days == 0 && hours == 1 && minutes < 0) return 60 + minutes + " minute" + ((minutes != 1) ? "s" : "") + " ago: 2";
+			else if(days == 0) return hours + " hour" + ((hours != 1) ? "s" : "") + " ago : 3";
+			else if(days == 1 && hours < 0) return 24 + hours + " hour" + ((hours != 1) ? "s" : "") + " ago : 4";
+
+			return destinationFormat.format(_timeStamp.getTime()) + " : 5";
 		}
 		catch(ParseException pe) { return "Time of Post Unavailable"; }
 	}
