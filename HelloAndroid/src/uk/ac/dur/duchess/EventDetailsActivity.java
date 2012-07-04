6394089863614dcd883badc9c234554914b7a30e
jamie_bates_8 <jamie_bates_8@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-04 14:55:10 +0000

    Updated event details screen

--- HelloAndroid/src/uk/ac/dur/duchess/EventDetailsActivity.java
+++ HelloAndroid/src/uk/ac/dur/duchess/EventDetailsActivity.java
@@ -19,7 +19,9 @@ public class EventDetailsActivity extends Activity
 	private TextView txtName;
 	private TextView txtDate;
 	private TextView txtDescription;
-	private TextView txtContact;
+	private Button phoneContactButton;
+	private Button emailContactButton;
+	private Button viewWebsiteButton;
 	private Button facebookButton;
 	private LinearLayout eventDetailsContainer;
 
@@ -32,7 +34,9 @@ public class EventDetailsActivity extends Activity
 		txtName = (TextView) findViewById(R.id.textViewEventName);
 		txtDate = (TextView) findViewById(R.id.textViewEventDate);
 		txtDescription = (TextView) findViewById(R.id.textViewEventDescription);
-		txtContact = (TextView) findViewById(R.id.textViewContactDetails);
+		phoneContactButton = (Button) findViewById(R.id.telephoneButton);
+		emailContactButton = (Button) findViewById(R.id.emailContactButton);
+		viewWebsiteButton = (Button) findViewById(R.id.websiteButton);
 		facebookButton = (Button) findViewById(R.id.facebookButton);
 		eventDetailsContainer = (LinearLayout) findViewById(R.id.eventDetailsContainer);
 
@@ -58,21 +62,21 @@ public class EventDetailsActivity extends Activity
 		String description = e.getString("event_description");
 		String contactTelephone = e.getString("event_contact_telephone_number");
 		String contactEmail = e.getString("event_contact_email_address");
+		String webAddress = e.getString("event_web_address");
 		String image_url = e.getString("image_url");
 
 		if (name != null) txtName.setText(name);
 		if (start_date != null && end_date != null) txtDate.setText(date);
 		if (description != null) txtDescription.setText(description);
-
-		String contactDetails = "";
-
-		if (contactTelephone == null && contactEmail == null)
-			 contactDetails = "No contact details available";
-		else contactDetails = "For more information: "
-			 + ((contactTelephone != null) ? "Call: " + contactTelephone + "\n" : "")
-			 + ((contactEmail != null) ? "Email: " + contactEmail : "");
 		
-		txtContact.setText(contactDetails);
+		if(contactTelephone != null) phoneContactButton.setText(contactTelephone);
+		else phoneContactButton.setVisibility(View.GONE);
+		
+		if(contactEmail != null) emailContactButton.setText(contactEmail);
+		else emailContactButton.setVisibility(View.GONE);
+		
+		if(webAddress != null) viewWebsiteButton.setText(webAddress);
+		else viewWebsiteButton.setVisibility(View.GONE);
 
 		(new DownloadImageTask()).execute(image_url);
 	}
