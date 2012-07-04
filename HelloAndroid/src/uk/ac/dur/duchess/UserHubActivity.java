6bfed002ec45ef3c25f6e70a24053031fb4d071b
matthew117 <matthew117@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-04 12:01:43 +0000

    Added toasts to the user hub to explain what each thing does.

--- HelloAndroid/src/uk/ac/dur/duchess/UserHubActivity.java
+++ HelloAndroid/src/uk/ac/dur/duchess/UserHubActivity.java
@@ -5,13 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserHubActivity extends Activity
 {
 	private TextView browseButton;
 	private TextView collegeEventButton;
 	private TextView myEventsButton;
-	private TextView newsButtons;
+	private TextView newsButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -22,7 +23,7 @@ public class UserHubActivity extends Activity
 		browseButton = (TextView) findViewById(R.id.userHubBrowse);
 		collegeEventButton = (TextView) findViewById(R.id.userHubCollege);
 		myEventsButton = (TextView) findViewById(R.id.userHubMyEvents);
-		newsButtons = (TextView) findViewById(R.id.userHubNews);
+		newsButton = (TextView) findViewById(R.id.userHubNews);
 		
 		browseButton.setOnClickListener(new View.OnClickListener()
 		{		
@@ -31,6 +32,34 @@ public class UserHubActivity extends Activity
 			{
 				Intent i = new Intent(v.getContext(), MainActivity.class);
 				startActivity(i);				
+			}
+		});
+		
+		collegeEventButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Toast.makeText(v.getContext(), "Displays a list of events run by the user's college", Toast.LENGTH_LONG).show();
+				
+			}
+		});
+		
+		myEventsButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Toast.makeText(v.getContext(), "Displays a list of events that the user is going to", Toast.LENGTH_LONG).show();				
+			}
+		});
+		
+		newsButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Toast.makeText(v.getContext(), "Displays a news feed about Durham events", Toast.LENGTH_LONG).show();				
 			}
 		});
 
