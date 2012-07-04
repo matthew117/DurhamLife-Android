0cf73d9164cf617a2a5d56c8fd5f94a0ef6b20d8
matthew117 <matthew117@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-04 11:10:04 +0000

    Changed user hub to use a TableLayout rather than a GridView.

--- HelloAndroid/src/uk/ac/dur/duchess/UserHubActivity.java
+++ HelloAndroid/src/uk/ac/dur/duchess/UserHubActivity.java
@@ -3,58 +3,34 @@ package uk.ac.dur.duchess;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
+import android.widget.TextView;
 
 public class UserHubActivity extends Activity
 {
-	private static final int BROWSE_EVENTS = 0;
-	private static final int COLLEGE_EVENTS = 1;
-	private static final int USER_EVENTS = 2;
-	private static final int NEWS = 3;
-
-	private GridView grid;
+	private TextView browseButton;
+	private TextView collegeEventButton;
+	private TextView myEventsButton;
+	private TextView newsButtons;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.user_hub);
-
-		grid = (GridView) findViewById(R.id.userHubGridView);
-
-		Integer[] iconIDs = new Integer[] { R.drawable.grid, R.drawable.college, R.drawable.star,
-				R.drawable.news };
-		String[] textLabels = new String[] { "Browse", "College Events", "My Events", "News" };
-
-		grid.setAdapter(new ImageGridAdapter(this, iconIDs, textLabels));
-
-		grid.setOnItemClickListener(new OnItemClickListener()
-		{
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+		setContentView(R.layout.user_hub_table);
+		
+		browseButton = (TextView) findViewById(R.id.userHubBrowse);
+		collegeEventButton = (TextView) findViewById(R.id.userHubCollege);
+		myEventsButton = (TextView) findViewById(R.id.userHubMyEvents);
+		newsButtons = (TextView) findViewById(R.id.userHubNews);
+		
+		browseButton.setOnClickListener(new View.OnClickListener()
+		{		
+			@Override
+			public void onClick(View v)
 			{
-				switch (position)
-				{
-				case BROWSE_EVENTS:
-					Intent i = new Intent(v.getContext(), MainActivity.class);
-					startActivity(i);
-					break;
-				case COLLEGE_EVENTS:
-					Toast.makeText(v.getContext(), "College Events", Toast.LENGTH_LONG).show();
-					break;
-				case USER_EVENTS:
-					Toast.makeText(v.getContext(), "Your Events", Toast.LENGTH_LONG).show();
-					break;
-				case NEWS:
-					Toast.makeText(v.getContext(), "News", Toast.LENGTH_LONG).show();
-					break;
-				default:
-					Log.e("UserHubActivity", "This shouldn't happen");
-				}
+				Intent i = new Intent(v.getContext(), MainActivity.class);
+				startActivity(i);				
 			}
 		});
 
