ad3d8e736da436ce7ad82361949ed61737157455
jamie_bates_8 <jamie_bates_8@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-09 10:04:19 +0000

    Improved appearance of calendar date selector

--- HelloAndroid/src/uk/ac/dur/duchess/activity/CalendarActivity.java
+++ HelloAndroid/src/uk/ac/dur/duchess/activity/CalendarActivity.java
@@ -2,23 +2,23 @@ package uk.ac.dur.duchess.activity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 public class CalendarActivity extends Activity
 {
-	private TableLayout table;
 	private Calendar calendar;
 	private int lowerBound;
 	private int upperBound;
@@ -32,14 +32,38 @@ public class CalendarActivity extends Activity
 		super.onCreate(savedInstanceState);
 		
 		layout = new LinearLayout(this);
-		table = new TableLayout(this);
 		calendar = Calendar.getInstance();
 		
 		setMonthBounds(calendar.get(Calendar.MONTH));
 		
 		int cell = 1 - lowerBound;
 		
-		TableRow days = new TableRow(this);
+		LinearLayout header = new LinearLayout(this);
+		header.setLayoutParams(new LinearLayout.LayoutParams(-1, -2, 0));
+		header.setOrientation(1);
+		
+		TextView monthText = new TextView(this);
+		
+		SimpleDateFormat month = new SimpleDateFormat("MMMMM yyyy");
+		
+		monthText.setText(month.format(calendar.getTime()));
+		monthText.setTextColor(Color.BLACK);
+		monthText.setTextSize(15);
+		monthText.setTypeface(Typeface.SERIF, Typeface.BOLD);
+		monthText.setPadding(5, 5, 5, 5);
+		monthText.setGravity(Gravity.CENTER_HORIZONTAL);
+		
+		GradientDrawable gradient = new GradientDrawable(Orientation.BOTTOM_TOP,
+			new int[] {Color.parseColor("#7E317B"), Color.parseColor("#D8ACE0")});
+		gradient.setDither(true);
+		
+		header.setBackgroundDrawable(gradient);
+		
+		header.addView(monthText);
+		
+		
+		LinearLayout days = new LinearLayout(this);
+		days.setLayoutParams(new LinearLayout.LayoutParams(-1, -2, 0));
 		
 		String[] dayStrings = {"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"};
 		
@@ -47,38 +71,34 @@ public class CalendarActivity extends Activity
 		{
 			TextView day = new TextView(this);
 			day.setText(dayStrings[i]);
-			days.addView(day);
+			day.setGravity(Gravity.CENTER);
+			day.setPadding(0, 0, 0, 5);
+			day.setTextColor(Color.BLACK);
+			days.addView(day, new LinearLayout.LayoutParams(0, -2, 1));
 		}
 		
-		table.addView(days);
+		header.addView(days);
+		
+		layout.addView(header);
 		
 		for(int row = 0; row < 6; row++)
 		{
-			TableRow tableRow = new TableRow(this);
+			LinearLayout tableRow = new LinearLayout(this);
+			tableRow.setLayoutParams(new LinearLayout.LayoutParams(-1, -2, 0));
 			
 			for(int col = 0; col < 7; col++)
 			{
-				if(cell < 1) tableRow.addView(getMonthButton(lastBound + cell, Color.LTGRAY));
-				else if(cell > upperBound) tableRow.addView(getMonthButton(cell % upperBound, Color.LTGRAY));
-				else tableRow.addView(getMonthButton(cell, Color.BLACK));
+				if(cell < 1) tableRow.addView(getMonthButton(lastBound + cell, Color.GRAY), new LinearLayout.LayoutParams(0, -2, 1)); 
+				else if(cell > upperBound) tableRow.addView(getMonthButton(cell % upperBound, Color.GRAY), new LinearLayout.LayoutParams(0, -2, 1));
+				else tableRow.addView(getMonthButton(cell, Color.BLACK), new LinearLayout.LayoutParams(0, -2, 1));
 				
 				cell++;
 			}
 			
-			table.addView(tableRow);
+			layout.addView(tableRow);
 		}
 		
-		TextView monthText = new TextView(this);
-		
-		SimpleDateFormat month = new SimpleDateFormat("MMMMM");
-		
-		monthText.setText(month.format(calendar.getTime()));
-		
 		layout.setOrientation(1);
-		layout.setGravity(Gravity.CENTER_HORIZONTAL);
-		
-		layout.addView(monthText);
-		layout.addView(table);
 		
 		setContentView(layout);
 		
@@ -88,7 +108,7 @@ public class CalendarActivity extends Activity
 	{
 		CalendarButton button = new CalendarButton(this, color);
 		
-		button.setText(String.valueOf(day));
+		button.setText(String.valueOf(((day < 10) ? " " : "") + day));
 		button.setTextColor(color);
 		button.setBackgroundColor(Color.WHITE);
 		
