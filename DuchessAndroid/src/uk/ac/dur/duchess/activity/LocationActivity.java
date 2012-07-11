163de34fe1246f2ddd133ae1063ba87c1a78c826
jamie_bates_8 <jamie_bates_8@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-11 22:15:51 +0000

    Added compass

--- DuchessAndroid/src/uk/ac/dur/duchess/activity/LocationActivity.java
+++ DuchessAndroid/src/uk/ac/dur/duchess/activity/LocationActivity.java
@@ -10,8 +10,14 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -27,7 +33,7 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 
-public class LocationActivity extends MapActivity
+public class LocationActivity extends MapActivity implements SensorEventListener
 {
 	private GeoPoint point;
 	private GeoPoint currentLocation;
@@ -37,7 +43,12 @@ public class LocationActivity extends MapActivity
 	private TextView addressBlock;
 	private TextView eventName;
 	
+	private SensorManager mySensorManager;
+	private Sensor mySensor;
+
 	private Bitmap compass;
+	private double distance = 0;
+	private float rotation = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -47,7 +58,7 @@ public class LocationActivity extends MapActivity
 
 		addressBlock = (TextView) findViewById(R.id.addressBlockTextView);
 		eventName = (TextView) findViewById(R.id.locationEventNameLabel);
-		
+
 		compass = BitmapFactory.decodeResource(getResources(), R.drawable.arrow);
 
 		Bundle e = getIntent().getExtras();
@@ -86,7 +97,25 @@ public class LocationActivity extends MapActivity
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 		mapView.invalidate();
+
+		mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+	
+		mySensor = mySensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 	}
+
+
+	protected void onResume()
+	{
+		super.onResume();
+		mySensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	protected void onPause()
+	{
+		super.onPause();
+		mySensorManager.unregisterListener(this);
+	}
+
 
 	@Override
 	protected boolean isRouteDisplayed()
@@ -107,62 +136,72 @@ public class LocationActivity extends MapActivity
 			Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.arrow);
 			canvas.drawBitmap(bmp, screenPoint.x - 36, screenPoint.y - 50, null);
 			
+
 			Display display = getWindowManager().getDefaultDisplay(); 
 			int width = canvas.getWidth();
 			int height = canvas.getHeight();
-			
+
 			int x = width - 100;
 			int y = height - 400;
 			int bx = x + (compass.getWidth()  / 2);
 			int by = y + (compass.getHeight() / 2);
-			
+
 			Matrix matrix = new Matrix();
 			matrix.reset();
 			matrix.setTranslate(x, y);
 			matrix.postRotate(getRotation(), bx, by);
 			
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.STROKE);
+			paint.setStrokeWidth(3);
+			paint.setColor(Color.BLACK);
+			paint.setTextSize(30);
+			
+			canvas.drawText(String.valueOf(distance), bx, by, paint);
 			canvas.drawBitmap(compass, matrix, null);
 
 			return true;
 		}
 	}
-	
+
 	private float getRotation()
 	{
 		if(currentLocation != null)
 		{
 			float[] d = {(float) ((point.getLongitudeE6() / 1E6) - (currentLocation.getLongitudeE6() / 1E6)),
-						 (float) ((point.getLatitudeE6()  / 1E6) - (currentLocation.getLatitudeE6()  / 1E6))};
-			
+					(float) ((point.getLatitudeE6()  / 1E6) - (currentLocation.getLatitudeE6()  / 1E6))};
+
 			Log.d("LOCATION", d[0] + ", " + d[1]);
-			
+
 			double angle = getAngle(d, new float[] {0, 1}); //smallest angle between translation vector and north
-			
+
 			if(d[1] < 0) angle = 180 - angle; //if the latitude direction is -ve (south)
 			if(d[0] < 0) angle = -angle; //if the longitude direction is -ve (west)
-			
-			return (float) angle + 180;
+
+
+
+			return (float) angle + 180 + rotation;
 		}
 		else return 0;
 	}
-	
+
 	public static double getAngle(float[] u, float[] v)
 	{
 		return acos(dot(u, v) / (sqrt(dot(u, u)) * sqrt(dot(v, v))));
 	}
-	
+
 	public static float orient2D(float[] a, float[] b, float[] c)
 	{
 		return (a[0] - c[0]) * (b[1] - c[1]) - (a[1] - c[1]) * (b[0] - c[0]);
 	}
-	
+
 	public static float dot(float[] u, float[] v)
 	{
 		float dot = 0;
 		int n = u.length;
-		
+
 		for(int i = 0; i < n; i++) dot += (u[i] * v[i]);
-		
+
 		return dot;
 	}
 
@@ -176,11 +215,6 @@ public class LocationActivity extends MapActivity
 				currentLocation = new GeoPoint((int) (loc.getLatitude() * 1E6),
 						(int) (loc.getLongitude() * 1E6));
 
-				Toast.makeText(
-						getApplicationContext(),
-						currentLocation.getLatitudeE6() / 1E6 + ", "
-								+ currentLocation.getLongitudeE6() / 1E6, Toast.LENGTH_LONG).show();
-
 				double R = 6371.0; // km
 				double dLat = Math.toRadians((point.getLatitudeE6() / 1E6 - currentLocation.getLatitudeE6() / 1E6));
 				double dLon = Math.toRadians((point.getLongitudeE6() / 1E6 - currentLocation.getLongitudeE6() / 1E6));
@@ -190,10 +224,7 @@ public class LocationActivity extends MapActivity
 				double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2)
 						* Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
 				double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-				double d = R * c;
-
-				Toast.makeText(getApplicationContext(), "DISTANCE : " + d, Toast.LENGTH_LONG)
-						.show();
+				distance = R * c;
 			}
 		}
 
@@ -211,6 +242,18 @@ public class LocationActivity extends MapActivity
 		public void onStatusChanged(String provider, int status, Bundle extras)
 		{
 		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1)
+	{
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event)
+	{
+		rotation = (float) event.values[0];
 	}
 
 }
