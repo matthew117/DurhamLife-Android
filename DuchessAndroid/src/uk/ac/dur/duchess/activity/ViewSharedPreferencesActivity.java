4f204bbaca22e1fb3adf6e6d4ecc0a62decebf15
matthew117 <matthew117@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-12 15:28:40 +0000

    Added debugging screen to view shared preferences.

--- DuchessAndroid/src/uk/ac/dur/duchess/activity/ViewSharedPreferencesActivity.java
+++ DuchessAndroid/src/uk/ac/dur/duchess/activity/ViewSharedPreferencesActivity.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import uk.ac.dur.duchess.R;
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,22 +12,33 @@ import android.widget.TextView;
 public class ViewSharedPreferencesActivity extends Activity
 {
 	private TextView output;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.view_sharded_preferences);
-		
+
 		output = (TextView) findViewById(R.id.sharedPreferenceOutput);
-		
-		SharedPreferences sp = getSharedPreferences("UserSession", Activity.MODE_PRIVATE);
-		
+
+		SharedPreferences sp = getSharedPreferences("UserSession", Context.MODE_PRIVATE);
+
 		Map<String, ?> map = sp.getAll();
-		
+
 		for (String key : map.keySet())
 		{
-			output.append(key + " : " + map.get(key)+"\n");
+			output.append(key + " : " + map.get(key) + "\n");
+		}
+		
+		output.append("\n");
+
+		SharedPreferences fb = getSharedPreferences("facebook-credentials", Context.MODE_PRIVATE);
+
+		Map<String, ?> newMap = fb.getAll();
+
+		for (String key : newMap.keySet())
+		{
+			output.append(key + " : " + newMap.get(key) + "\n");
 		}
 	}
 }
