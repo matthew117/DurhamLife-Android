05ded95bfa5abec675655e28e010ff74dc7e1289
matthew117 <matthew117@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-07-18 15:31:16 +0000

    Removed unused imports.

--- DuchessAndroid/src/uk/ac/dur/duchess/activity/SocietyEventListActivity.java
+++ DuchessAndroid/src/uk/ac/dur/duchess/activity/SocietyEventListActivity.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
