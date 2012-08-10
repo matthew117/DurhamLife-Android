2a2f665c30a435f99e1964af90e4954077e434cb
jamie_bates_8 <jamie_bates_8@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-08-10 08:54:27 +0000

    Removed old psuedo-title bar from society event list

--- DuchessAndroid/src/uk/ac/dur/duchess/activity/SocietyEventListActivity.java
+++ DuchessAndroid/src/uk/ac/dur/duchess/activity/SocietyEventListActivity.java
@@ -8,13 +8,9 @@ import uk.ac.dur.duchess.R;
 import uk.ac.dur.duchess.data.NetworkFunctions;
 import uk.ac.dur.duchess.data.SessionFunctions;
 import uk.ac.dur.duchess.entity.User;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
@@ -24,12 +20,8 @@ public class SocietyEventListActivity extends SherlockActivity
 {
 	private EventListView listView;
 	private User user;
-	private TextView societyNameText;
 	
 	private long societyID;
-
-	private Button aboutButton;
-	private Button subscribeButton;
 
 	private String societyName;
 	
@@ -44,38 +36,13 @@ public class SocietyEventListActivity extends SherlockActivity
 		context = this;
 		
 		user = SessionFunctions.getCurrentUser(this);
-		
-		societyNameText = (TextView) findViewById(R.id.nameOnSocietyEventList);
-
-		aboutButton = (Button) findViewById(R.id.societyAboutButton);
-		subscribeButton = (Button) findViewById(R.id.societyButtonSubscribe);
 
 		final Bundle s = getIntent().getExtras();
-
-		aboutButton.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				Intent i = new Intent(v.getContext(), SocietyAboutActivity.class);
-				Bundle societyInfo = getIntent().getExtras();
-
-				i.putExtra("society_id", societyInfo.getString("society_id"));
-				i.putExtra("society_name", societyInfo.getString("society_name"));
-				i.putExtra("society_constitution", societyInfo.getString("society_constitution"));
-				i.putExtra("society_website", societyInfo.getString("society_website"));
-				i.putExtra("society_email", societyInfo.getString("society_email"));
-
-				startActivity(i);
-
-			}
-		});
 
 		if (s.getString("society_name") != null)
 		{
 			setTitle(s.getString("society_name"));
 			societyName = s.getString("society_name");
-			societyNameText.setText(societyName);
 		}
 		
 		if (s.getLong("society_id") != -1)
@@ -89,52 +56,6 @@ public class SocietyEventListActivity extends SherlockActivity
 		b.putString("society_name", societyName);
 		
 		listView.loadAllEvents(this, b);
-
-		if(user.isSubscribedToSociety(societyNameText.getText().toString()))
-			subscribeButton.setText("Unsubscribe");
-
-		subscribeButton.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				String societyName = societyNameText.getText().toString();
-
-				if(!user.isSubscribedToSociety(societyName))
-				{
-					Thread t = new Thread(new Runnable()
-					{		
-						@Override
-						public void run()
-						{
-							try
-							{
-								InputStream is = NetworkFunctions.getHTTPResponseStream(
-										"http://www.dur.ac.uk/cs.seg01/duchess/api/v1/societies.php?userID=" +
-												user.getUserID() + "&societyID=" + s.getLong("society_id"), "GET", null);
-							}
-							catch (IOException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					});
-					t.start();
-
-					user.getSocieties().add(societyNameText.getText().toString());
-					subscribeButton.setText("Unsubscribe");
-				}
-				else
-				{
-					// TODO Should also update in the external database not just in the shared preferences 
-					user.getSocieties().remove(societyNameText.getText().toString());
-					subscribeButton.setText("Subscribe");
-				}
-
-				SessionFunctions.saveUserPreferences((Activity) v.getContext(), user);
-			}
-		});
 	}
 
 	@Override
@@ -170,11 +91,22 @@ public class SocietyEventListActivity extends SherlockActivity
 		switch (item.getItemId())
 		{
 		case R.id.showAboutSocietyMenuItem:
-			Intent showAboutBoxIntent = new Intent(this, SocietyAboutActivity.class);
-			startActivity(showAboutBoxIntent);
-			return true;
-		case R.id.subscribeSocietyMenuItem:
+		{
+			Intent i = new Intent(context, SocietyAboutActivity.class);
+			Bundle societyInfo = getIntent().getExtras();
 
+			i.putExtra("society_id", societyInfo.getString("society_id"));
+			i.putExtra("society_name", societyInfo.getString("society_name"));
+			i.putExtra("society_constitution", societyInfo.getString("society_constitution"));
+			i.putExtra("society_website", societyInfo.getString("society_website"));
+			i.putExtra("society_email", societyInfo.getString("society_email"));
+
+			startActivity(i);
+			
+			return true;
+		}
+		case R.id.subscribeSocietyMenuItem:
+		{
 			if(!user.isSubscribedToSociety(societyName))
 			{
 				Thread t = new Thread(new Runnable()
@@ -211,8 +143,9 @@ public class SocietyEventListActivity extends SherlockActivity
 
 			SessionFunctions.saveUserPreferences(this, user);
 			return true;
-		default:
-			return true;
+		}
+		
+		default: return true;
 		}
 	}
 }
