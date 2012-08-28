7054cbc8f96fe038a12e6ab3783d5dbff4638729
jamie_bates_8 <jamie_bates_8@da4b984a-175e-40bd-bed4-c8b2bed055ea>
2012-08-28 09:24:29 +0000

    Fixed 'empty views' for society lists, changed user hubs to use purple buttons and Browse to Events, changed maps api key

--- DuchessAndroid/src/uk/ac/dur/duchess/ui/activity/PersonalSocietyListActivity.java
+++ DuchessAndroid/src/uk/ac/dur/duchess/ui/activity/PersonalSocietyListActivity.java
@@ -16,5 +16,6 @@ public class PersonalSocietyListActivity extends BaseActivity
 
 		listView = (SocietyListView) findViewById(R.id.societyListView);
 		listView.loadSocieties(this);
+		listView.setEmptyView(findViewById(R.id.mySocietyListEmpty));
 	}
 }
